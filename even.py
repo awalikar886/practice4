102d8805ffd54e844a95721b88036837a4e7ea3f
awalikar886 <awalikar886@gmail.com>
2025-11-27 15:42:42 +0530

    Update even.py

--- even.py
+++ even.py
@@ -1,11 +1,15 @@
 import sys
 
+if len(sys.argv) > 1:
+    script_name = sys.argv[0]
+    numbers = sys.argv[1:]
+    print("User provided input values:")
+else:
+    script_name = sys.argv[0]
+    numbers = ["10", "21", "32", "45", "50"]
+    print("No input given — using default values:")
 
-if len(sys.argv) != 2:
-    print("Usage: python even_odd_counter.py \"1 2 3 4 5\"")
-    sys.exit(1)
-
-numbers = list(map(int, sys.argv[1].split()))
+numbers = [int(n) for n in numbers]
 
 even_count = 0
 odd_count = 0
@@ -16,5 +20,8 @@ for n in numbers:
     else:
         odd_count += 1
 
-print("Even numbers count:", even_count)
-print("Odd numbers count:", odd_count)
+print("\n--- Even and Odd Count ---")
+print("Script Name:", script_name)
+print("Numbers:", numbers)
+print("Even Numbers Count:", even_count)
+print("Odd Numbers Count:", odd_count)
